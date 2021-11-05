96a8475c82edf2be62a6cb3e14c47dcb1d4d35c0
Laura Contreras <m.laura.cp89@gmail.com>
2021-11-05 11:18:35 +0100

    hola

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    let tasks = [];
+    $('#submit').on("click", function() {
+        let newTask = $('#newTask').val();
+        
+
+        if (newTask !== "") {
+
+            tasks.push(newTask);
+
+            $('#taskList').empty()
+
+            tasks.forEach(t =>
+                $('#taskList').append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+
+    })
+    $('#clearButton').on("click", function () {
+        if (newTask !== "") {
+
+            tasks.splice(newTask);}
+
+            $('#taskList').empty()
+        })
+});
+
